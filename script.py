c1ce168456390f72ce4fd9e4369cb3f6000aeee2
Serhan Aya <serhane@gmail.com>
2023-09-06 19:51:55 +0300

    First commit

---
+++ script.py
@@ -0,0 +1,14 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+if __name__ == '__main__':
+    print('Success')
